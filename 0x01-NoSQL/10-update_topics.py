2bb2d0fedc07c881c0dc0867892b3e9e5cd5375f
nandz080 <nandz080@gmail.com>
2024-04-30 11:20:02 -0700

    [task 10] updated solution

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,6 +2,8 @@
 """ MongoDB Operations with Python using pymongo """
 
 def update_topics(mongo_collection, name, topics):
-    """Updates topics of a school document based on the name"""
-    # Update the topics of the school document with the provided name
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    """ Changes all topics of a school document based on the name """
+    schoolName = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+
+    mongo_collection.update_many(schoolName, new_values)
